c08fd79624de29f67deab3f5b792e6d90a24963a
Max <maxchabanov2009@gmail.com>
2021-09-09 20:11:22 +0300

    changed path in files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './login-form/auth/auth.component';
-import { RegisterComponent } from './login-form/register/register.component';
+import { AuthComponent } from './login/auth/auth.component';
+import { RegisterComponent } from './login/register/register.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthComponent } from './login-form/auth/auth.component';
-import { RegisterComponent } from './login-form/register/register.component';
-import { ForgotPasswordComponent } from './login-form/forgot-password/forgot-password.component';
+import { AuthComponent } from './login/auth/auth.component';
+import { RegisterComponent } from './login/register/register.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, RegisterComponent, ForgotPasswordComponent],
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
