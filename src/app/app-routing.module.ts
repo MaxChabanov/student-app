d8ba389f331570fec5905b8d236228a86bc6eb6d
Max <maxchabanov2009@gmail.com>
2021-09-09 20:05:40 +0300

    fixed router issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './login-form/auth/auth.component';
+import { RegisterComponent } from './login-form/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'auth', component: AuthComponent },
+  // { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './login-form/auth/auth.component';
+import { RegisterComponent } from './login-form/register/register.component';
+import { ForgotPasswordComponent } from './login-form/forgot-password/forgot-password.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AuthComponent, RegisterComponent, ForgotPasswordComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
