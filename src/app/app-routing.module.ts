e4bc48651f03c12943a4524d61735dbde9f24dcb
Ruslan <58141049+WoodsOfTroy@users.noreply.github.com>
2021-09-13 09:50:24 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './login/auth/auth.component';
+import { RegisterComponent } from './login/register/register.component';
+import { RegisterStudentComponent } from './login/register-student/register-student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'register-student', component: RegisterStudentComponent },
+  // { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstNameInputComponent } from './components/first-name-input/first-name-input.component';
-import { SecondNameInputComponent } from './components/second-name-input/second-name-input.component';
-import { EmailInputComponent } from './components/email-input/email-input.component';
-import { PassInputComponent } from './components/pass-input/pass-input.component';
-import { ConfirmPassInputComponent } from './components/confirm-pass-input/confirm-pass-input.component';
-import { RegBtnComponent } from './components/reg-btn/reg-btn.component';
-import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
-import { WrapperComponent } from './base/wrapper/wrapper.component';
-import { BackgroundComponent } from './base/background/background.component';
+import { AuthComponent } from './login/auth/auth.component';
+import { RegisterComponent } from './login/register/register.component';
+import { ForgotPasswordComponent } from './login/forgot-password/forgot-password.component';
+import { RegisterStudentComponent } from './login/register-student/register-student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstNameInputComponent,
-    SecondNameInputComponent,
-    EmailInputComponent,
-    PassInputComponent,
-    ConfirmPassInputComponent,
-    RegBtnComponent,
-    RegistrationFormComponent,
-    WrapperComponent,
-    BackgroundComponent
+    AuthComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    RegisterStudentComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
