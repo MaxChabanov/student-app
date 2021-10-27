4ccb5d747430801f7871bccfb38f347ab729ecd1
Max <maxchabanov2009@gmail.com>
2021-10-27 14:29:41 +0300

    setup heroku server js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/student-app"));
+app.use(express.static(__dirname + "/dist/student-appj"));
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/student-app/index.html"));
