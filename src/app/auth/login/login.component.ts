149616ee367675f795523058cee245c3f67074d4
Max <maxchabanov2009@gmail.com>
2021-11-08 16:54:37 +0200

    need help with http get

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  login() {
+    this.http.get(url:'assets/user-accounts.json')
+  }
 
   ngOnInit(): void {}
 }
