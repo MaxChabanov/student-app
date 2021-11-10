b3c69badd65ab80a406146d76914cfa67164d5ff
Max <maxchabanov2009@gmail.com>
2021-11-10 19:28:30 +0200

    logic for snackbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 // Material
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
 
 const materialArray = [
   MatAutocompleteModule,
   MatFormFieldModule,
   MatSelectModule,
   MatInputModule,
+  MatSnackBarModule,
+  MatButtonModule,
 ];
 
 @NgModule({
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.scss'],
 })
 export class StudentComponent implements OnInit {
-  constructor() {}
   ngOnInit(): void {}
+
+  constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar() {
+    this._snackBar.open('Test message', 'Ok', {
+      horizontalPosition: 'end',
+      verticalPosition: 'bottom',
+    });
+  }
 }
