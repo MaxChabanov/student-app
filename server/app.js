ea6f24779a0112eac5b44f9a86871da9927de18a
Alexey Solovei <soloveyalexey3@gmail.com>
2021-12-09 19:02:12 +0200

    added mongo conection

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const authRoute = require('./routes/auth');
+const authModule = require('./modules/auth');
 
 const app = express();
+
+authModule.main().catch(console.error);
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
